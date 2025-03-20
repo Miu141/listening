6272dc55685f89b9c75d4abb0d74b206d09c9fba
kuroro-31 <shun.ichiro.tanaka16@gmail.com>
2025-03-21 07:49:58 +0900

    デプロイエラー修正

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -400,7 +400,7 @@ const ResultPage = () => {
           <h2 className="font-bold text-lg mb-3">問題と解説</h2>
           <div className="space-y-8">
             {audioData.questions.map((question, index) => {
-              const isCorrect =
+              const _isCorrect =
                 selectedAnswers[index] === question.correctAnswer;
               return (
                 <div
